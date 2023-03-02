071606f2d7cd5f1f650576bd049d50d52d4f6c53
franciapareja <francia.pareja@gmail.com>
2023-03-02 08:43:41 -0500

    n4secrea ejemplos de ciclos y listas

---
+++ ciclofor.py
@@ -0,0 +1,8 @@
+for controlador in range(10):
+    print(controlador)
+
+
+#otro ejemplo : (desde donde, hasta donde, rango)
+   
+for controlador in range(0, 200, 12):
+    print(controlador)
---
+++ lista1.py
@@ -0,0 +1,15 @@
+#lista de datos (almacenas muchos datos en una variable)
+numeros=[1,2,3,4,5]
+
+#acceder a todos los datos de la lista al mismo tiempo
+print(numeros)
+
+#acceder a un solo elemento de la lista
+print(numeros[0])
+
+#acceder de forma individual a cada elemento de una lista
+for numero in numeros:
+    print(numero)
+    
+for numero in numeros:
+    print(numero+10)
